c5db492d5ac55696d9b3c2c1f545672ddadae609
mazhar75 <mazharulnihadmd@gmail.com>
2024-07-11 23:46:28 +0600

    4th iteration

--- Web Application/BooksDukan/Models/Category.cs
+++ Web Application/BooksDukan/Models/Category.cs
@@ -1,9 +1,13 @@
-namespace BooksDukan.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace BooksDukan.Models{
 
 public class Category{
+    [Key]
     public int Id { get; set; }
 
     [Required]
     public string Name { get; set; }
     public int DisplayOrder{ get; set; }
 } 
+}
