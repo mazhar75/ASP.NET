6268e0f760885d07cd7e5d8682b00127b60415ee
mazhar75 <mazharulnihadmd@gmail.com>
2024-07-12 15:32:44 +0600

    5th Iteration

---
+++ Web Application/BooksDukan/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+using BooksDukan.Data;
+using BooksDukan.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace BooksDukan.Controllers;
+
+public class CategoryController : Controller
+{  
+    private readonly ApplicationDbContext _db;
+    public CategoryController(ApplicationDbContext db)
+    {
+            _db=db;
+    }
+    public IActionResult Index()
+    {   
+        List<Category> objCategoryList = _db.Categories.ToList();
+        return View(objCategoryList);
+    }
+}
