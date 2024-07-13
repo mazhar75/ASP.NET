19d35c8584f69a7766b4043c1d9b7ab8752ed274
mazhar75 <mazharulnihadmd@gmail.com>
2024-07-13 15:05:19 +0600

    Iteration 7

--- Web Application/BooksDukan/Controllers/CategoryController.cs
+++ Web Application/BooksDukan/Controllers/CategoryController.cs
@@ -24,8 +24,63 @@ public class CategoryController : Controller
     [HttpPost]
      public IActionResult Create(Category obj)
     {
+        if(ModelState.IsValid)
+        {
         _db.Categories.Add(obj);
         _db.SaveChanges();
         return RedirectToAction("Index");
+        }
+        return View();
+
+    }
+     public IActionResult Edit(int? id)
+    {  
+        if(id==null || id==0){
+            return NotFound();
+        }
+        Category category = _db.Categories.Find(id);
+
+        if(category == null){
+            return NotFound();
+        }
+        return View(category);
+    }
+    [HttpPost]
+     public IActionResult Edit(Category obj)
+    {
+        if(ModelState.IsValid)
+        {
+        _db.Categories.Update(obj);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+        }
+        return View();
+
+    }
+     public IActionResult Delete(int? id)
+    {  
+        if(id==null || id==0){
+            return NotFound();
+        }
+        Category category = _db.Categories.Find(id);
+
+        if(category == null){
+            return NotFound();
+        }
+        return View(category);
+    }
+    [HttpPost, ActionName("Delete")]
+     public IActionResult DeletePOST(int? id)
+    {   
+        Category? obj = _db.Categories.Find(id);
+        if(obj == null){
+
+            return NotFound();
+            
+        }
+        _db.Categories.Remove(obj);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+
     }
 }
